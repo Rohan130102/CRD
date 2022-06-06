f36a0b09fb4e1d5b2f543f39db009a83f31e5ac8
pitubit <paruipritam67@gmail.com>
2022-06-06 16:16:40 +0530

    Pritam setup Commit

--- src/main/java/com/dailywork/pojo/User.java
+++ src/main/java/com/dailywork/pojo/User.java
@@ -1,5 +1,42 @@
 package com.dailywork.pojo;
 
-public class User {
+import java.util.Set;
 
+public class User {
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Set<Task> getTasks() {
+		return tasks;
+	}
+	public void setTasks(Set<Task> tasks) {
+		this.tasks = tasks;
+	}
+	private String password;
+	private String email;
+	private Set<Task> tasks;
 }
